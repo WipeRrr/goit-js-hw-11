a4370f0089ddfcb10b5c2a55b4c88464ab05dabc
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-03-24 20:58:18 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './js/fetchImages';
 
 const inputEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreEl = document.querySelector(`.load-more`);
 
 inputEl.addEventListener('submit', onSearch);
 
+loadMoreEl.addEventListener('click', onLoadMore);
+
+let name = '';
+
 function onSearch(e) {
   e.preventDefault();
-  const name = e.currentTarget.searchQuery.value;
+  name = e.currentTarget.searchQuery.value;
 
   if (name === '') {
     return;
   }
-  console.log(name);
   e.currentTarget.reset();
+  fetchImages(name)
+    .then(renderImg)
+    .catch(error => {
+      console.log(error);
+    });
+  clearGallery();
+  loadMoreEl.classList.remove(`is-hidden`);
+}
+
+function onLoadMore() {
   fetchImages(name)
     .then(renderImg)
     .catch(error => {
@@ -25,7 +40,13 @@ function onSearch(e) {
 }
 
 function renderImg(data) {
-  const markup = data
+  console.log(data);
+  if (data.hits.length === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  const markup = data.hits
     .map(
       ({
         webformatURL,
@@ -35,21 +56,23 @@ function renderImg(data) {
         views,
         comments,
         downloads,
+        pageURL,
       }) => {
         return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags} " loading="lazy" />
+        <a class="gallery__item" href="${largeImageURL}">
+  <img class="gallery__image" src="${webformatURL}" alt="${tags} " loading="lazy" /> </a>
   <div class="info">
     <p class="info-item">
-      <b>Likes ${likes}</b>
+      <b>Likes<span class="img_stats">${likes}</span></b>
     </p>
     <p class="info-item">
-      <b>Views ${views}</b>
+      <b>Views<span class="img_stats">${views}</span></b>
     </p>
     <p class="info-item">
-      <b>Comments ${comments}</b>
+      <b>Comments<span class="img_stats">${comments}</span></b>
     </p>
     <p class="info-item">
-      <b>Downloads ${downloads}</b>
+      <b>Downloads<span class="img_stats">${downloads}</span></b>
     </p>
   </div>
 </div>
@@ -59,4 +82,11 @@ function renderImg(data) {
     .join('');
 
   galleryEl.insertAdjacentHTML(`beforeend`, markup);
+  new SimpleLightbox('.photo-card a', {
+    captionDelay: 250,
+  });
+}
+
+function clearGallery() {
+  galleryEl.innerHTML = '';
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,24 @@
-export const fetchImages =  name => {
+import axios from 'axios';
+
+export const fetchImages = name => {
   const BASE_URL = 'https://pixabay.com/api/';
+  let page = 1;
 
   const searchParams = new URLSearchParams({
-    key:'33763391-fe078dc9f17400c9e34720d71',
+    key: '33763391-fe078dc9f17400c9e34720d71',
     q: name,
     image_type: `photo`,
     orientation: `horizontal`,
     safesearch: `true`,
+    page: page,
   });
 
-  return fetch(`${BASE_URL}?${searchParams}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
+  return axios
+    .get(`${BASE_URL}?${searchParams}&per_page=40`)
+    .then( data  => {
+      page += 1;
+      return data;
+    })
     .catch(error => {
       console.error(error);
     });
